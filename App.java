fbb7d443b670387408480987acf4205d487402e3
Aadesh744 <aadeshy744@gmail.com>
2024-08-21 23:16:36 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("enter the number");
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the num1");
+        int a=sc.nextInt();
+        System.out.println("enter the num2");
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+
     }
 }
