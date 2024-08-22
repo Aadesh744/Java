dc49413b19558112d45afb155884cbe453276d9f
Aadesh744 <aadeshy744@gmail.com>
2024-08-23 00:12:16 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,17 +1,15 @@
-
 public class App {
-    public static void main(String[] args) throws Exception {
-        int a =10;
-        int b =20;
-        if(a+b < 10){
-            System.out.println("a+b is less than 10");
-        }
-            else {
-                System.out.println("a+b is greater than 10");
-            }
+    public static void main(String[] args) {
+        String city = "chandrapur";
+
+        if (city.equals("kathmandu")) { 
+            System.out.println("city is kathmandu");
+        } else if (city.equals("nepalgunj")) { 
+            System.out.println("city is nepalgunj");
+        } else if (city.equals("santapur")) { 
+            System.out.println("city is santapur");
+        } else {
+            System.out.println("city"); 
         }
     }
-        
-
-
- 
+}
