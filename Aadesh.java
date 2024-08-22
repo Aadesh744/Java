27de5d571fc63ddf9ddf5ee40ce01a4efa192241
Aadesh744 <aadeshy744@gmail.com>
2024-08-22 22:59:39 +0530

    Add files via upload

---
+++ Aadesh.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Aadesh {
+    public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number:");
+    int N =sc.nextInt();
+    System.out.println("You entered:" +N);
+    }
+}
+
