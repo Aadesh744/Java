a71e8330f69e2bbc957bc043ffd9b343b2ac6b95
Aadesh744 <aadeshy744@gmail.com>
2024-08-24 22:39:00 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,15 +1,17 @@
 public class App {
-    public static void main(String[] args) {
-        String city = "chandrapur";
-
-        if (city.equals("kathmandu")) { 
-            System.out.println("city is kathmandu");
-        } else if (city.equals("nepalgunj")) { 
-            System.out.println("city is nepalgunj");
-        } else if (city.equals("santapur")) { 
-            System.out.println("city is santapur");
-        } else {
-            System.out.println("city"); 
-        }
+    public static void main(String[] args) throws Exception {
+        //creating strings
+     String str1 ="Aadesh Yadav";
+     String str2 ="Address";
+     String str3 ="Chandrapur";
+     //printing string 
+     System.out.println(str1);
+     System.out.println(str2);
+     System.out .println(str3);
+     //length of the string 
+     System.out.println("length of str1:"+str1.length());
+     System.out.println("length of str2:"+str2.length());
+     System.out.println("length of str3:"+str3.length());
+     
     }
 }
