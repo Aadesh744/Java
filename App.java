06bba2cc113acb83a78ef593d9e1430b933b7438
Aadesh744 <aadeshy744@gmail.com>
2024-08-24 23:16:51 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,17 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        //creating strings
-     String str1 ="Aadesh Yadav";
-     String str2 ="Address";
-     String str3 ="Chandrapur";
-     //printing string 
-     System.out.println(str1);
-     System.out.println(str2);
-     System.out .println(str3);
-     //length of the string 
-     System.out.println("length of str1:"+str1.length());
-     System.out.println("length of str2:"+str2.length());
-     System.out.println("length of str3:"+str3.length());
-     
-    }
+  //creating strings
+  String str1 ="Aadesh Yadav";
+  String str2 ="Bikesh yadav";
+  String str3 ="coimbatore";
+  //character  at specifier index
+  System.out.println("character at index 5 is:" +str1.charAt(5));
+  System.out.println("character at index 4 is:" +str2.charAt(4));
+  System.out.println("character at index 7 is:" +str3.charAt(7));
+  //Substring 
+  System.out.println("Substring of str1 from index 0 to 12:"+str1.substring(0, 12));
+  System.out.println("Substring of str2 from index 0 to 12:"+str2.substring(0, 12));
+  System.out.println("Substring of str3 from index 0 to 10:"+str3.substring(0, 10));
+  }
 }
