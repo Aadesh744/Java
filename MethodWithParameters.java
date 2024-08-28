edd43903535d94050b5a5f99d9216ec30c63eee5
Aadesh744 <aadeshy744@gmail.com>
2024-08-28 23:59:29 +0530

    Add files via upload

---
+++ MethodWithParameters.java
@@ -0,0 +1,12 @@
+public class MethodWithParameters {
+    public static void main(String[] args) {
+        printSum(5, 10);
+    }
+
+    public static void printSum(int a, int b) {
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
+
+
