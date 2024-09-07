a0bd715577055edd9f3f9397e38336c5de9540d2
Aadesh744 <aadeshy744@gmail.com>
2024-09-08 00:17:10 +0530

    Add files via upload
    opps code 2

--- Aadesh.java
+++ Aadesh.java
@@ -1,11 +1,33 @@
-public class Aadesh {
-    public static void main(String[] args) {
-        int i = 1;
-        while (i <= 5) {
-            System.out.println("Number: " + i);
-            i++;
-        }
+class Pen {
+    String colour;
+    String type; // ballpen, gelpen
+
+    // Using Function
+    public void write() {
+        System.out.println("I am writing");
+    }
+
+    public void printColour() {
+        System.out.println(this.colour);
+    }
+
+    public void printType() {
+        System.out.println(this.type);
     }
 }
 
+public class Aadesh {
+    public static void main(String[] args) {
+        // Creating objects
+        Pen pen1 = new Pen();
+        pen1.colour = "black";
+        
+        Pen pen2 = new Pen();
+        pen2.type = "gelpen";
+        
+        pen1.printColour();
+        
+        pen2.printType();
+    }
+}
 
