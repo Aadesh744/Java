f95efc53e1dee2a5fceff0e15cc4434db151c747
Aadesh744 <aadeshy744@gmail.com>
2024-09-09 23:31:03 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,29 +1,11 @@
-class Student{
-    String name;
-    int age;
-    //Creating function 
-    public void printinfo(String name){
-      System.out.println(name);
-    }
-    public void printInfo(int age){
-        System.out.println(age);
-    }
-    public void printInfo(String name,int age){
-        System.out.println(name+" "+age);
-    }
-
-}
-
-
 public class App {
-    public static void main(String[] args) throws Exception {
-        Student s1= new Student();
-        s1.name="aadesh";
-        s1.age=20;
-        s1.printInfo(s1.age);
-        s1.printInfo(s1.name,s1.age);
-
-
-       
+    public static void main(String[] args) {
+        int a = 20, b = 0, c;
+        try {
+            c = a / b;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
