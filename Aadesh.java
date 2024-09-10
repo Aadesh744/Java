e31e09ce72edd4935c33590c2fa88d1461f09c41
Aadesh744 <aadeshy744@gmail.com>
2024-09-10 23:42:15 +0530

    Add files via upload

--- Aadesh.java
+++ Aadesh.java
@@ -1,33 +1,14 @@
-class Pen {
-    String colour;
-    String type; // ballpen, gelpen
-
-    // Using Function
-    public void write() {
-        System.out.println("I am writing");
-    }
-
-    public void printColour() {
-        System.out.println(this.colour);
-    }
-
-    public void printType() {
-        System.out.println(this.type);
-    }
-}
+import java.util.ArrayList;
 
 public class Aadesh {
     public static void main(String[] args) {
-        // Creating objects
-        Pen pen1 = new Pen();
-        pen1.colour = "black";
-        
-        Pen pen2 = new Pen();
-        pen2.type = "gelpen";
-        
-        pen1.printColour();
-        
-        pen2.printType();
+        ArrayList<Integer> number = new ArrayList<>();
+        number.add(5);
+        number.add(6);
+        number.add(7);
+        number.add(8);
+        number.add(9);
+        number.add(10);
+        System.out.println(number);
     }
 }
-
