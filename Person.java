e81ea2f4031a02c33c9e39cfe910f48fc03240ee
Aadesh744 <aadeshy744@gmail.com>
2024-09-14 23:21:42 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        
+        Person person = new Person("Aadesh", 20);
+        person.displayInfo();
+    }
+}
+
