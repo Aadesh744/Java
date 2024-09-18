2ede7b127833fec094a90dba7f84e9f61c281892
Aadesh744 <aadeshy744@gmail.com>
2024-09-18 09:40:11 +0530

    Add files via upload

---
+++ inheritance1.java
@@ -0,0 +1,16 @@
+class shape{
+    String colour;
+}
+
+class traingle extends shape{
+
+}
+
+
+public class inheritance1{
+    public static void main(String[] args) throws Exception {
+    traingle t1 = new traingle();
+    t1.colour = "red";
+  System.out.println(t1.colour);
+    }
+}
