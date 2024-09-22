9d60dfba38e26084c6abfc1ff813bb9e3ac617af
Aadesh744 <aadeshy744@gmail.com>
2024-09-22 23:54:23 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Arraylist{
+    public static void main(String[] args) {
+        ArrayList<String> studentNames = new ArrayList<>(); 
+
+        studentNames.add("Aadesh");
+        studentNames.add("Bikesh");
+        studentNames.add("Nikhil");
+        studentNames.add("Prabej");
+        studentNames.add("Rajbir");
+
+        System.out.println(studentNames); 
+    }
+}
