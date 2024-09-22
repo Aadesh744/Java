16791b99c6677b9f40e3d837db3b374341fbd99e
Aadesh744 <aadeshy744@gmail.com>
2024-09-22 22:21:57 +0530

    Add files via upload

--- aadesh.java
+++ aadesh.java
@@ -1,23 +1,19 @@
-Class pen {
+class colourtype{
     String colour;
-    String type;//ballpen,gelpen
-    //Using Function
-    public void write(){
-        System.out.println("i am writung");
-    }
-    public void printcolour(){
-     System.out.println("this.colour");
-    }
-
 }
-public class aadesh {
-    public static void main(String[] args) throws Exception {
-        //Creating object
-        pen.pen1=new Pen();
-        pen1.colour="black";
-        pen1.type="gelpen";
-        pen1.colour();
-
-
-    }
+class shape extends colourtype{
+   String shape;
+}
+class random extends shape{
+   
+}
+public class aadesh{ 
+public static void main(String [] args ){
+   //creating object
+   random r1=new random();
+   r1.colour="red";
+   r1.shape="rectangele";
+   System.out.println(r1.colour);
+   System.out.println(r1.shape);
+}
 }
