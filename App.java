cea9511aaf35214ef11d1ed721ebe250a77f9fff
Aadesh744 <aadeshy744@gmail.com>
2024-09-27 14:41:08 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,27 +1,16 @@
-import java.util.ArrayList;
+import java.util.Stack;
 
 public class App {
     public static void main(String[] args) throws Exception {
-      ArrayList<String>studentname=new ArrayList<>();
-      studentname.add("aadesh");
-      studentname.add("bikesh");
-      studentname.add("nikhil");
-      studentname.add("rajbir");
-      System.out.println(studentname);
-      studentname.add("ram");
-      System.out.println(studentname);
-      //using remove function to remove object aadesh from the student name list
-      studentname.remove("aadesh");
-      System.out.println(studentname);
-      //using set function 
-      studentname.set(0,"aadeshyadav");
-      System.out.println(studentname);
-      //using contain function to check wheather the student name is contain in list or not
-     studentname.contains("aadesh");
-     System.out.println(studentname);
-      //using clear function to clear all the studentname 
-      studentname.clear();
-      System.out.println(studentname);
-
+     Stack<Integer>studentroll=new Stack<>();
+     studentroll.push(11);
+     studentroll.push(12);
+     studentroll.push(13);
+     studentroll.push(14);
+     studentroll.push(15);
+     System.out.println("studentroll at top: "+studentroll.peek());
+     System.out.println("removed studentroll:"+studentroll.pop() );
+     
+    
     }
 }
