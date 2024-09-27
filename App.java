a764804a5178d7370f128f34acdf059f6e73d829
Aadesh744 <aadeshy744@gmail.com>
2024-09-27 14:08:23 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+
 public class App {
-    public static void main(String[] args) {
-        int a = 20, b = 0, c;
-        try {
-            c = a / b;
-            System.out.println(c);
-        } catch (ArithmeticException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+    public static void main(String[] args) throws Exception {
+      ArrayList<String>studentname=new ArrayList<>();
+      studentname.add("aadesh");
+      studentname.add("bikesh");
+      studentname.add("nikhil");
+      studentname.add("rajbir");
+      System.out.println(studentname);
+      studentname.add("ram");
+      System.out.println(studentname);
+      //using remove function to remove object aadesh from the student name list
+      studentname.remove("aadesh");
+      System.out.println(studentname);
+      //using set function 
+      studentname.set(0,"aadeshyadav");
+      System.out.println(studentname);
+      //using contain function to check wheather the student name is contain in list or not
+     studentname.contains("aadesh");
+     System.out.println(studentname);
+      //using clear function to clear all the studentname 
+      studentname.clear();
+      System.out.println(studentname);
+
     }
 }
