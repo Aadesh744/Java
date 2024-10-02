2ad64f838c97450192d7992ddcb230b423ac6964
Aadesh744 <aadeshy744@gmail.com>
2024-10-02 23:27:10 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,49 +1,17 @@
-import java.util.ArrayList;
-
-class Stack<T> {
-    private ArrayList<T> stack;
-
-    public Stack() {
-        stack = new ArrayList<>();
-    }
-
-    public void push(T item) {
-        stack.add(item);
-    }
-
-    public T pop() {
-        if (isEmpty()) {
-            throw new RuntimeException("Stack is empty");
-        }
-        return stack.remove(stack.size() - 1);
-    }
-
-    public T peek() {
-        if (isEmpty()) {
-            throw new RuntimeException("Stack is empty");
-        }
-        return stack.get(stack.size() - 1);
-    }
-
-    public boolean isEmpty() {
-        return stack.isEmpty();
-    }
-
-    public int size() {
-        return stack.size();
-    }
-
+public class App {
     public static void main(String[] args) {
-        Stack<Integer> intStack = new Stack<>();
         
-        intStack.push(1);
-        intStack.push(2);
-        intStack.push(3);
-
-        System.out.println("Top element: " + intStack.peek());
-        System.out.println("Stack size: " + intStack.size());
-
-        System.out.println("Popped element: " + intStack.pop());
-        System.out.println("Stack size after pop: " + intStack.size());
+        int[] numbers = {10, 20, 30, 40, 50};
+        
+        
+        int sum = 0;
+        
+        
+        for(int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        
+        
+        System.out.println("The sum of the numbers is: " + sum);
     }
 }
