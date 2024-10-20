f3b50d1d8a50654336b9a0e8c73cf8ace565bfec
Aadesh744 <aadeshy744@gmail.com>
2024-10-20 18:24:34 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,11 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-//Creating string
-String str1 = "kathmandu";
-String str2 = "coimbatore";
-String str3 = "Tamilnadu";
-//Concatenation 
-String concatenation = str1+" "+str2;  
-System.out.println("concatenation of strings:"+concatenation);
-}
+    public static void main(String[] args)  {
+        System.out.println(System.currentTimeMillis());
+    }
 }
