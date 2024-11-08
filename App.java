a15b42e6176ba52ddddcacc800a0e79145fe8c86
Aadesh744 <aadeshy744@gmail.com>
2024-11-08 22:30:41 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,29 +1,5 @@
-import java.util.Stack;
-
 public class App {
-    public static void main(String[] args) {
-      
-        Stack<String> stack = new Stack<>();
-
-      
-        stack.push("Element 1");
-        stack.push("Element 2");
-        stack.push("Element 3");
-
-      
-        System.out.println("display Top element in stack: " + stack.peek());
-
-  
-        System.out.println("remove/pop element which is at top " + stack.pop());
-
-        System.out.println(" display element value which is at top: " + stack.peek());
-
-       
-        while (!stack.isEmpty()) {
-            System.out.println("pop : " + stack.pop());
-        }
-
-      
-        System.out.println("empty? " + stack.isEmpty());
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
