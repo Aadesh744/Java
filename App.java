357d7849f817b1f24a478f7fa23106b6b0f3fa82
Aadesh744 <aadeshy744@gmail.com>
2024-11-10 22:05:19 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,5 +1,25 @@
+
+interface Animal {
+    void eat();
+}
+interface Mammal {
+    void walk();
+}
+class Dog implements Animal, Mammal {
+
+    public void eat() {
+        System.out.println("Dog is eating.");
+    }
+     public void walk() {
+        System.out.println("Dog is walking.");
+    }
+}
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        
+        dog.eat();  
+        dog.walk(); 
     }
 }
