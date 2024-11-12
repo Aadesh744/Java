d6216ad022bf10edfcbbd182bdfde712135efadd
Aadesh744 <aadeshy744@gmail.com>
2024-11-12 22:18:27 +0530

    Add files via upload

---
+++ app.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+
+public class app{
+    public static void main(String[] args) throws Exception {
+       
+        ArrayList<int>number=new ArrayList<>();
+        number.add(5);
+        number.add(10);
+        number.add(15);
+        number.add(20);
+        System.out.print(number);
+              
+    }
+}
