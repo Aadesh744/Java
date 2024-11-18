82b164f10851f95e6ffddde9a0ab5eea4db70ffc
Aadesh744 <aadeshy744@gmail.com>
2024-11-18 23:35:06 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,33 +1,11 @@
-class aadesh{
-    String name;
-    double regno;
-    aadesh(String name,double regno){
-        this.name=name;
-        this.regno=regno;
-    }
-    aadesh(bikesh b1){
-        this.name=b1.name;
-        this.regno=b1.regno;
-    }
-    public void printInfo(){
-        System.out.println(this.name);
-        System.out.println(this.regno);
-    }
-    
-}
-
-
-
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-aadesh.a1=new aadesh(ram,211);
-aadesh.b1=new aadesh(a1);
-//calling the function
-a1.printInfo();
-b1.printInfo();
-
-
+//Creating string
+String str1 = "kathmandu";
+String str2 = "coimbatore";
+String str3 = "Tamilnadu";
+//Concatenation 
+String concatenation = str1+" "+str2;  
+System.out.println("concatenation of strings:"+concatenation);
 }
-
+}
