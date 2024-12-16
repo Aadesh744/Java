d88d2c1b7d670a22ae89494a450b06c5f001ede6
Aadesh744 <aadeshy744@gmail.com>
2024-12-16 22:44:31 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,5 +1,17 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        int[] numbers = {10, 20, 30, 40, 50};
+        
+        
+        int sum = 0;
+        
+        
+        for(int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        
+        
+        System.out.println("The sum of the numbers is: " + sum);
     }
 }
