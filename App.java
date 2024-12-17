f5bbec573ef3eeb4662d5f3f6acaed76fd60d237
Aadesh744 <aadeshy744@gmail.com>
2024-12-17 22:11:49 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,17 +1,46 @@
-public class App {
-    public static void main(String[] args) {
-        
-        int[] numbers = {10, 20, 30, 40, 50};
-        
-        
-        int sum = 0;
-        
-        
-        for(int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
+class PrintNumbers extends Thread {
+    private String threadName;
+
+   
+    public PrintNumbers(String threadName) {
+        this.threadName = threadName;
+    }
+
+    
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            try {
+                
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+            System.out.println("Thread " + threadName + " - " + i);
         }
-        
-        
-        System.out.println("The sum of the numbers is: " + sum);
+    }
+}
+
+public class App{
+    public static void main(String[] args) {
+      
+        PrintNumbers thread1 = new PrintNumbers("A");
+        PrintNumbers thread2 = new PrintNumbers("B");
+        PrintNumbers thread3 = new PrintNumbers("C");
+
+      
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        try {
+            
+            thread1.join();
+            thread2.join();
+            thread3.join();
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+
+        System.out.println("All threads have finished.");
     }
 }
