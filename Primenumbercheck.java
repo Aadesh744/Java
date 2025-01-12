e95b11222405a8dafbb9f1cfe7d02b8aa78749da
Aadesh744 <aadeshy744@gmail.com>
2025-01-12 23:22:23 +0530

    Add files via upload

---
+++ Primenumbercheck.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Primenumbercheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number! ✨");
+        } else {
+            System.out.println(num + " is not a prime number. 😔");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
