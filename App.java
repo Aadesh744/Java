64beeba44e2e65a1e46623dfa6e761a979cc7dad
Aadesh744 <aadeshy744@gmail.com>
2025-02-15 23:00:51 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,5 +1,16 @@
+import java.util.Stack;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     Stack<Integer>studentroll=new Stack<>();
+     studentroll.push(11);
+     studentroll.push(12);
+     studentroll.push(13);
+     studentroll.push(14);
+     studentroll.push(15);
+     System.out.println("studentroll at top: "+studentroll.peek());
+     System.out.println("removed studentroll:"+studentroll.pop() );
+     
+    
     }
 }
