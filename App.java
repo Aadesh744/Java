ca0e2c20685d25badd693b5ff669ae49dd912793
Aadesh744 <aadeshy744@gmail.com>
2025-02-17 23:26:05 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,16 +1,33 @@
-import java.util.Stack;
+class aadesh{
+    String name;
+    double regno;
+    aadesh(String name,double regno){
+        this.name=name;
+        this.regno=regno;
+    }
+    aadesh(bikesh b1){
+        this.name=b1.name;
+        this.regno=b1.regno;
+    }
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.regno);
+    }
+    
+}
+
+
 
 public class App {
     public static void main(String[] args) throws Exception {
-     Stack<Integer>studentroll=new Stack<>();
-     studentroll.push(11);
-     studentroll.push(12);
-     studentroll.push(13);
-     studentroll.push(14);
-     studentroll.push(15);
-     System.out.println("studentroll at top: "+studentroll.peek());
-     System.out.println("removed studentroll:"+studentroll.pop() );
-     
-    
+        System.out.println("Hello, World!");
     }
+aadesh.a1=new aadesh(ram,211);
+aadesh.b1=new aadesh(a1);
+//calling the function
+a1.printInfo();
+b1.printInfo();
+
+
 }
+
